fe1e912748582ae8374097ae4745e02312e157e2
mahzabinrashid <mahzabinrashidfariha@gmail.com>
2024-05-18 00:28:59 -0400

    init commit

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// CORS
+app.use(cors());
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
+
+// Define a simple route
+app.get("/api", (req, res) => {
+  res.send("Hello, MERN!");
+});
+
+// Define a route to fetch data
+app.get("/data", (req, res) => {
+  const data = [
+    { id: 1, name: "Item 1", description: "This is item 1" },
+    { id: 2, name: "Item 2", description: "This is item 2" },
+  ];
+  res.json(data);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
