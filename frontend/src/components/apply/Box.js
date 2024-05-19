f9e148d3511925ba6c4bf5e40aac736bca664c21
mahzabinrashid <mahzabinrashidfariha@gmail.com>
2024-05-19 01:11:28 -0400

    updated apply boxes

--- frontend/src/components/apply/Box.js
+++ frontend/src/components/apply/Box.js
@@ -2,19 +2,22 @@ import React from "react";
 import "./Box.scss";
 import AmazonLogo from "../../assets/images/amazon.png";
 import Bookmark from "../../assets/icons/bookmark.svg";
+
 export default function Box() {
   return (
     <div className="box">
-      <div className="box__header">
-        <img src={AmazonLogo} alt="Amazon logo" className="box__logo" />
-        <img src={Bookmark} alt="Bookmark" className="box__bookmark" />
-      </div>
-      <div className="box__body">
-        <h3 className="box__company">Amazon</h3>
-        <p className="box__description">
-          Design a modern, minimalist logo for Amazon Payment.
-        </p>
-        <p className="box__date">18 May, 2023</p>
+      <div className="box__content">
+        <div className="box__header">
+          <img src={AmazonLogo} alt="Amazon logo" className="box__logo" />
+          <img src={Bookmark} alt="Bookmark" className="box__bookmark" />
+        </div>
+        <div className="box__body">
+          <h3 className="box__company">Amazon</h3>
+          <p className="box__description">
+            Design a modern, minimalist logo for Amazon Payment.
+          </p>
+          <p className="box__date">18 May, 2023</p>
+        </div>
       </div>
       <div className="box__footer">
         <p className="box__reward">$15,000</p>
